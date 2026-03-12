8c4325ce850d462f66a94505f392ab5dce67190d
1301315780-svg <1301315780@s.tusd1.org>
2026-03-12 21:57:21 +0000

    progress

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 console.log("Script started")
 
+let gueses = 0;
 function flipCard(cardId) {
     console.log("click");
     let card = document.getElementById(cardId);
     card.style.color = "white";
     card.style.backgroundColor = "green";
+
+
+    if (firstCardId = "") {
+        firstCardId = cardId;
+    console.log("first = " + firstCardId);
+    }
+    //store 1st card id
+    else {
+    secondCardId - cardId;
+    setTimeout(checkForMatch, 1000);
+    console.log("second = " + secondCardId);
+    }
+}
+
+function checkForMatch() {
+    //get the card elements
+    let firstCard = document.getElementById(firstCardId);
+    let secondCard = document.getElementById(secondCardId)
+
+    if (firstCard.InnerText == secondCard.InnerText) {
+        firstCard.style.backgroundColor = "lightgreen";
+        secondCard.style.backgroundColor = "lightgreen";
+    }
+    else {
+        // Cards don't match, reset the styles
+        firstCard.style.color = "yellow";
+        firstCard.style.backgroundColor = "yellow"
+         secondCard.style.color = "yellow";
+        secondCard.style.backgroundColor = "yellow"
+    }
+    //RESET FOR NEXT PICK
+    firstCardId = "";
+    secondCardId = "";
 }
